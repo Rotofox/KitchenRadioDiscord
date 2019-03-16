c5aa00e65a8d25d5cba0cb705cae2370dbec8bd2
unknown <alexriglea18@gmail.com>
2019-03-16 18:36:34 +0200

    v0.03 !commands and new user role setter

--- bot.py
+++ bot.py
@@ -10,12 +10,27 @@ TOKEN = 'NTUyODU4OTY0NTA1Nzg4NDM2.D2FrVg.tfvKUUmpX2NXnptPucDBlFzMlwo'
 client = commands.Bot(command_prefix = '!')
 
 @asyncio.coroutine
+
 @client.event
 # Prints to console that it's ready, has full functionality.
 # Function will run reagardless of what the bot is doing at the time.
 async def on_ready():
+    await client.change_presence(game=discord.Game(name='!commands'))
     print(client.user.name + ' is ready to chooch.')
 
+@client.event
+# Assigns a role to a new user on the server;
+# Choose which role you want a new user to have    name='YOUR ROLE'
+async def on_member_join(member):
+    role =  discord.utils.get(member.server.roles, name='Veggie Cutter')
+    await client.add_roles(member, role)
+
+@client.command()
+async def commands():
+    await client.say('!commands - Displays this list\n'
+                   + '!ping - Pong!\n'
+                   + '!clear - Clears the chat\n'
+                   + '!play - Plays a clip from Youtube')
 
 @client.command()
 async def ping():
