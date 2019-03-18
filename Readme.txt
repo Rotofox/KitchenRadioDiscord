87851bfc1c04cb3f4cc7552815684798e5bbf786
unknown <alexriglea18@gmail.com>
2019-03-18 08:53:55 +0200

    changed !command to !help & redirected it into a private message

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,8 @@
 Install the Discord.py library if you want to make changes to the bot
 https://github.com/Rapptz/discord.py
+[THERE'S A LOT TO DO FOR THIS BOT TO BE FULLY FUNCTIONAL]
+
+This is a Discord bot that is able to 
+    - Assign a role to a new user on the server.
+    - Display the commands list.
+    - Clear chat.
--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ TOKEN = 'NTUyODU4OTY0NTA1Nzg4NDM2.D2FrVg.tfvKUUmpX2NXnptPucDBlFzMlwo'
 # Any implemented command will need to be typed in chat
 # with the prefix stated here. (!play, !stop) 
 client = commands.Bot(command_prefix = '!')
+client.remove_command('help') #removes the default help command; requires implemenentation of custom help command
 
 @asyncio.coroutine
 
@@ -16,7 +17,7 @@ client = commands.Bot(command_prefix = '!')
 # Function will run reagardless of what the bot is doing at the time.
 # Sets bot 'presence' in Discord  "BotName playing name='your message'"
 async def on_ready():
-    await client.change_presence(game=discord.Game(name='!command'))
+    await client.change_presence(game=discord.Game(name='!help'))
     print(client.user.name + ' is ready to chooch.')
 
 @client.event
@@ -26,17 +27,20 @@ async def on_member_join(member):
     role =  discord.utils.get(member.server.roles, name='Veggie Cutter')
     await client.add_roles(member, role)
 
-@client.command()
-async def command():
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
     embed = discord.Embed(
-        title = "Here's how you can use the bot:",
         color = discord.Color.gold()
     )
-    embed.add_field(name='!command', value='Displays this list', inline=False)
+    embed.set_author(name='Commands list:')
+    embed.add_field(name='!help', value="PM's you the commands list", inline=False)
     embed.add_field(name='!ping', value='Pong!', inline=False)
     embed.add_field(name='!clear', value='Clears the chat', inline=False)
     embed.add_field(name='!play', value='Plays a clip from Youtube', inline=False)
-    await client.say(embed=embed)
+    await client.send_message(author, embed=embed)
+
+
 
 @client.command()
 async def ping():
